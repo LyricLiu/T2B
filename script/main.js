9b59a0b24bd067489eb994b57c80b1106080802a
LyricLiu <yichiliu@foxmail.com>
2017-11-01 21:28:19 -0400

    add homepage
    add generate character logic

--- script/main.js
+++ script/main.js
@@ -1,6 +1,17 @@
 (function() {
+    //0 white; 1 yellow; 2 black
+    var skin_BOG = 0;
+    var skin_BOD = 0;
+    var handsome_BOG = 0;
+    var handsome_BOD = 0;
+    //0 white T-shirt; 1 white shirt; 2 white shirt with jacket; 
+    //3 professional; 4 actor; 5 musician; 6 exercise; 7 superman.
+    var cloth_BOG = 0;
+    var cloth_BOD = 0;
+    //0 handsome haircut 
+    var hair_BOG = 0;
+    var hair_BOD = 0;
 
-    var a = 1;
 
     $(document).ready(function() {
 
@@ -74,6 +85,100 @@
 	$('#status_BOD').selectize({
     	sortField: 'text'
 	});
+
+    //button control 
+
+    $('#create_story').click(function(){
+        $('#homepage').hide();
+        $('#design_character').show();
+    });
+
+    //select skin
+     $('#white_BOG').click(function(){
+        $('#white_BOG').css('border-bottom','2px solid #d74276');
+        $('#yellow_BOG').css('border-bottom','');
+        $('#black_BOG').css('border-bottom','');
+     })
+
+     $('#yellow_BOG').click(function(){
+        $('#yellow_BOG').css('border-bottom','2px solid #d74276');
+        $('#white_BOG').css('border-bottom','');
+        $('#black_BOG').css('border-bottom','');
+        var skin_BOG = 1;
+     })
+
+     $('#black_BOG').click(function(){
+        $('#black_BOG').css('border-bottom','2px solid #d74276');
+        $('#white_BOG').css('border-bottom','');
+        $('#yellow_BOG').css('border-bottom','');
+        var skin_BOG = 2;
+     })
+
+     $('#white_BOD').click(function(){
+        $('#white_BOD').css('border-bottom','2px solid #d74276');
+        $('#yellow_BOD').css('border-bottom','');
+        $('#black_BOD').css('border-bottom','');
+     })
+
+     $('#yellow_BOD').click(function(){
+        $('#yellow_BOD').css('border-bottom','2px solid #d74276');
+        $('#white_BOD').css('border-bottom','');
+        $('#black_BOD').css('border-bottom','');
+        var skin_BOD = 1;
+     })
+
+     $('#black_BOD').click(function(){
+        $('#black_BOD').css('border-bottom','2px solid #d74276');
+        $('#white_BOD').css('border-bottom','');
+        $('#yellow_BOD').css('border-bottom','');
+        var skin_BOD = 2;
+     })
+
+
+    //Generate character
+
+    $('#generate_character_button').click(function(){
+        if (($('#status_BOG').attr('value')=='clerk')||($('#status_BOG').attr('value')=='sale')){
+            cloth_BOG = 1;
+        }else if($('#status_BOG').attr('value')=='waiter'){
+            cloth_BOG = 2;
+        }else if(($('#status_BOG').attr('value')=='manager')||($('#status_BOG').attr('value')=='politician')){
+            cloth_BOG = 3;
+        }else if($('#status_BOG').attr('value')=='gangster'){
+            cloth_BOG = 5;
+        }else if($('#status_BOG').attr('value')=='coach'){
+            cloth_BOG = 6;
+        }else{
+            cloth_BOG = 0;
+        }
+
+        if($('#status_BOD').attr('value')=='clerk'){
+            cloth_BOD = 1;
+        }else if(($('#status_BOD').attr('value')=='entrepreneur')||($('#status_BOD').attr('value')=='billionaire')){
+            cloth_BOD = 2;
+        }else if(($('#status_BOD').attr('value')=='ceo')||($('#status_BOD').attr('value')=='politician')){
+            cloth_BOD = 3;
+        }else if($('#status_BOD').attr('value')=='actor'){
+            cloth_BOD = 4;
+        }else if($('#status_BOD').attr('value')=='rock'){
+            cloth_BOD = 5;
+        }else if(($('#status_BOD').attr('value')=='athlete')||($('#status_BOD').attr('value')=='fitness')){
+            cloth_BOD = 6;
+        }else if($('#status_BOD').attr('value')=='superman'){
+            cloth_BOD = 7;
+        }else{
+            cloth_BOD = 0;
+        }
+
+        if($('#slider_handsome_BOG').attr('value')<0){
+            hair_BOG = 1;
+        }
+
+        if($('#slider_handsome_BOD').attr('value')<0){
+            hair_BOD = 1;
+        }
+
+    })
 
 
 
